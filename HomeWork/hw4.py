db8d48b655a268588ed3ae2c199d0be347b4888f
Azimbek001 <azimmustapaev@gmail.com>
2023-05-18 19:17:34 +0300

    hw4

---
+++ HomeWork/hw4.py
@@ -0,0 +1,29 @@
+class Asset:
+    pass
+
+class InsurableItem:
+    pass
+
+class InterestBearingItem:
+    pass
+
+class BankAccount(InsurableItem, Asset, InterestBearingItem):
+    pass
+
+class SavingAccount(BankAccount):
+    pass
+
+class CheckingAccount(BankAccount):
+    pass
+
+class Security(InterestBearingItem, Asset):
+    pass
+
+class Stock(Security):
+    pass
+
+class RealEstate(Asset, InsurableItem):
+    pass
+
+class Bond(Security):
+    pass
--- month 2/lesson_3.py
+++ month 2/lesson_3.py
@@ -64,24 +64,3 @@
 #
 # print(dir(k))
 
-class Bank:
-    def __init__(self, name, balance):
-        self._name = name
-        self._balance = balance
-
-    def moneyX(self):
-        amount = float(input("Введите сумму для внесения на счет: "))
-        self._balance += amount
-        print(f"Ваш новый баланс: {self._balance}")
-
-    def _kill(self):
-        self._balance = 0
-        print(f"Ваш баланс обнулен: {self._balance}")
-
-    def __jackpot(self):
-        self._balance *= 10
-        print(f"Поздравляем! Ваш баланс увеличен в 10 раз: {self._balance}")
-
-    def merge_balance(self, other):
-        self._balance += other._balance
-        print(f"Ваш обновленный баланс после объединения: {self._balance}")
